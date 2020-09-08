616dc029538c5b2a9849e876279772b812b9b686
snezhok <66049865+snezhok-prog@users.noreply.github.com>
2020-09-08 23:09:34 +0300

    08.09.20
    homework with pascal, two-dimensional array.

---
+++ homework.pas
@@ -0,0 +1,37 @@
+﻿var arr: array[1..3,1..2] of integer;
+var arr_new: array[1..3,1..2] of integer;
+var i,k,less,most,l,r: integer;
+begin
+  for i:=1 to 3 do
+    for k:=1 to 2 do
+      arr[i,k]:= random (100);
+  
+  for i:=1 to 3 do
+  begin
+    for k:=1 to 2 do
+      write (arr[i,k], ' ');
+    writeln;
+  end;
+  less:= 200;
+  most:=-200;
+  for l:=1 to 3 do
+    for r:=1 to 2 do
+      begin
+        for i:=1 to 3 do
+          for k:=1 to 2 do
+            if (arr[i,k]<less) and (arr[i,k]>most) then
+              less:=arr[i,k];
+        arr_new[l,r]:=less;
+        most:=less;
+        less:=200;
+      end;
+  writeln;
+  writeln ('Отсортированный список');
+  writeln;
+  for i:=1 to 3 do
+  begin
+    for k:=1 to 2 do
+      write (arr_new[i,k], ' ');
+    writeln;
+  end;
+end.
